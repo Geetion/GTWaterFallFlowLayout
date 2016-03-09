b4f5c4c333f7ccf2672284030d3e213fe8fa3385
Geetion <ccgeetion@gmail.com>
2016-03-09 12:15:48 +0800

    first commit

--- GTWaterFallFlowLayout/ViewController.swift
+++ GTWaterFallFlowLayout/ViewController.swift
@@ -7,19 +7,125 @@
 //
 
 import UIKit
+import AFNetworking
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,WaterFallFlowLayoutDelegate,WaterFallFlowLayoutDatasource{
+    
+    var URL = "http://gank.io/api/data/福利/10/"
+    var imgArray = NSMutableArray()
+    var page = 2
+    
+    let wt = WaterFallFlowLayout()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.edgesForExtendedLayout = UIRectEdge.None
+        //        将url转换成utf-8
+        URL = URL.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        
+        requestImage()
+        
+        requestMoreImage()
+        
+        wt.initWithFrameRect(self.view.frame)
+        
+        self.view = wt
+        
+        wt.delegate = self
+        wt.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func requestImage(){
+        
+        let manager = AFHTTPSessionManager();
+        
+        let firstRequest:String = URL + "1"
+        
+        manager.GET(firstRequest, parameters: nil, progress: nil, success: { (nsurl:NSURLSessionDataTask, resp:AnyObject?) -> Void in
+            
+            let results = resp?.objectForKey("results") as! NSArray
+            
+            let currentArray = NSMutableArray()
+            
+            for each in results{
+                
+                let imgUrl = NSURL(string: (each["url"]!)! as! String)
+                
+                let data = NSData(contentsOfURL: imgUrl!)
+                
+                let image = UIImage(data: data!)
+                
+                currentArray.addObject(image!)
+                
+            }
+            
+            self.imgArray = currentArray
+            
+            self.wt.reloadData()
+            
+            }) { (nsurl:NSURLSessionDataTask?, error:NSError) -> Void in
+                
+        }
     }
-
-
+    
+    func requestMoreImage(){
+        
+        let manager = AFHTTPSessionManager();
+        
+        let requestUrl = URL + String(page)
+        
+        manager.GET(requestUrl, parameters: nil, progress: nil, success: { (nsurl:NSURLSessionDataTask, resp:AnyObject?) -> Void in
+            
+            let results = resp?.objectForKey("results") as! NSArray
+            
+            for each in results{
+                
+                let imgUrl = NSURL(string: (each["url"]!)! as! String)
+                
+                let data = NSData(contentsOfURL: imgUrl!)
+                
+                let image = UIImage(data: data!)
+                
+                self.imgArray.addObject(image!)
+                
+            }
+            
+            self.wt.reloadData()
+            
+            self.page++
+            }) { (nsurl:NSURLSessionDataTask?, error:NSError) -> Void in
+                
+        }
+    }
+    
+    func waterFallFlowLayout(didselectImageView indexPath: NSIndexPath) {
+        print(indexPath.row)
+    }
+    
+    func waterFallFlowLayout(numberOfItemsInSection section:Int)->Int{
+        return imgArray.count
+    }
+    
+    func waterFallFlowLayout(heightofItemAtIndexPath indexPath: NSIndexPath, itemWidth: CGFloat) -> CGFloat {
+        
+        let image = imgArray[indexPath.row]
+        
+        let newHeight = image.size.height/image.size.width * itemWidth
+        
+        return newHeight
+    }
+    
+    func waterFallFlowLayout(viewAtIndexPath indexPath:NSIndexPath)->UIView {
+        
+        let imageView = UIImageView(image: imgArray[indexPath.row] as? UIImage)
+        
+        return imageView
+    }
+    
+    func waterFallFlowLayout(numberOfColumnInSection section: Int) -> Int {
+        return 5
+    }
+    
 }
-
---
+++ Util/GTWaterFallFlowLayout.swift
@@ -0,0 +1,170 @@
+//
+//  WaterFallFlowLayout.swift
+//  For Code
+//
+//  Created by 胡健 on 26/02/2016.
+//  Copyright © 2016 GeekTeen. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol WaterFallFlowLayoutDatasource{
+    
+    func waterFallFlowLayout(numberOfItemsInSection section:Int)->Int
+    
+    func waterFallFlowLayout(heightofItemAtIndexPath indexPath:NSIndexPath,itemWidth:CGFloat)->CGFloat
+    
+    func waterFallFlowLayout(viewAtIndexPath indexPath:NSIndexPath)->UIView
+    
+    optional func waterFallFlowLayout(numberOfColumnInSection section:Int)->Int
+    
+    optional func waterFallFlowLayout(widthOfColumn section:Int)->CGFloat
+    
+    optional func waterFallFlowLayout()->NSSet
+}
+
+protocol WaterFallFlowLayoutDelegate{
+    
+    func waterFallFlowLayout(didselectImageView indexPath:NSIndexPath)
+    
+}
+
+class WaterFallFlowLayout: UIView{
+    
+    private var WIDTH:CGFloat?
+    
+    private var col:Int = 3
+    
+    private var cell_X = [CGFloat]()
+    
+    private var cell_Y = [CGFloat]()
+    
+    private var cellOrigin = NSMutableArray()
+    
+    private var scrollView = UIScrollView()
+    
+    var dataSource:WaterFallFlowLayoutDatasource?
+    
+    var delegate:WaterFallFlowLayoutDelegate?
+    
+    private var itemNumber = Int()
+    
+    func initWithFrameRect(frame:CGRect){
+        
+        self.frame = frame
+        
+        self.backgroundColor = UIColor.whiteColor()
+        
+        scrollView = UIScrollView(frame: frame)
+        scrollView.scrollEnabled = true
+        scrollView.bounces = true
+        
+        WIDTH = self.frame.width
+        
+        self.addSubview(scrollView)
+    }
+    
+    func getCellOrigin(){
+        
+        for(var i = 0;i<col;i++ ){
+            let newcell_X = self.WIDTH! / CGFloat(self.col) * CGFloat(i) + 5
+            cell_X.append(newcell_X)
+            
+            cell_Y.append(5)
+        }
+        
+    }
+    
+    func reloadData(){
+        
+        if(dataSource != nil ){
+            
+            itemNumber = (dataSource?.waterFallFlowLayout(numberOfItemsInSection: 0))!
+            
+            if let newCol = dataSource?.waterFallFlowLayout?(numberOfColumnInSection: 0){
+                col = newCol
+            }
+            
+            getCellOrigin()
+        }
+        
+        layoutView()
+    }
+    
+    func resetData(){
+
+    }
+    
+    func layoutView(){
+        
+        for (var i = 0;i<itemNumber;i++){
+            
+            let indexPath = NSIndexPath(forRow: i, inSection: 0)
+            
+            let imageView = dataSource?.waterFallFlowLayout(viewAtIndexPath: indexPath)
+            
+            imageView!.frame.size = sizeForItemAtIndexPath(i)
+            
+//            if i < cellOrigin.count{
+//                let size = CGPointFromString(cellOrigin[i] as! String)
+//                
+//                imageView!.frame.origin = CGPoint(x: size.x, y: size.y)
+//            }else{
+            
+                let currentCol = i % self.col
+                
+                imageView!.frame.origin = CGPoint(x: cell_X[currentCol], y: cell_Y[currentCol])
+                
+                cellOrigin.addObject(NSStringFromCGPoint(imageView!.frame.origin))
+                
+                cell_Y[currentCol] = cell_Y[currentCol] + imageView!.frame.height + 5
+//            }
+            
+            imageView!.tag = i
+            
+            let touch = UITapGestureRecognizer(target: self, action: "didSelectImageView:")
+            
+            imageView!.userInteractionEnabled = true
+            
+            imageView!.addGestureRecognizer(touch)
+            
+            self.scrollView.addSubview(imageView!)
+        }
+        
+        let maxHeight = getViewHeight()
+        
+        self.scrollView.contentSize = CGSize(width: WIDTH!, height: maxHeight)
+        
+    }
+    
+    func getViewHeight()->CGFloat{
+        
+        var maxHeight:CGFloat = 0
+        
+        for each in cell_Y{
+            
+            maxHeight = each > maxHeight ? each:maxHeight
+        }
+        
+        return maxHeight
+    }
+    
+    
+    func sizeForItemAtIndexPath(indexPath: Int) -> CGSize {
+        
+        let newindexPath = NSIndexPath(forRow: indexPath, inSection: 0)
+        
+        let newWidth = (WIDTH! - 20)/CGFloat(col)
+        
+        let newHeight = dataSource?.waterFallFlowLayout(heightofItemAtIndexPath: newindexPath, itemWidth: newWidth)
+        
+        return CGSize(width: newWidth, height: newHeight!)
+    }
+    
+    func didSelectImageView(tap:UIGestureRecognizer){
+        
+        let indexPath = NSIndexPath(forRow: tap.view!.tag, inSection: 0)
+        
+        delegate?.waterFallFlowLayout(didselectImageView: indexPath)
+    }
+}
